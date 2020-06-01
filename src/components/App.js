ce4ee0d279e05c33e45ccd4de30539c0f1d21b43
Kaan Yilmaz <douscriptist@gmail.com>
2020-06-01 05:17:17 +0300

    init react-electron todologger

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className='app'>
+			<h1>TodoLogger</h1>
+			<p>This is a simple boilerplate for using TodoLogger</p>
+		</div>
+	);
+};
+
+export default App;
